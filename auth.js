d859d1f89239c52b41f3a20078f7ddbd92d89bdd
vieconnect <nhattiento2704@gmail.com>
2026-02-08 01:11:00 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // Dữ liệu người dùng mẫu
 const users = {
-    'nhatnam-0888363955@tio.com': { password: 'Nhatnam1511@', name: 'Nguyễn Nhật Nam', sbd: '238659', birthday: '15/11/2015', class: 'Biên tập viên', work:'Tự soạn một bài thuyết trình về bản tin các quốc gia sau đó quay video gửi qua email Giám đốc', status:'Chưa hoàn thành', expiredate:'12h00 sáng, ngày 8/2/2026'}, 
+    'namnguyen9323': { password: 'Nhatnam1511@', name: 'Nguyễn Nhật Nam', sbd: '238659', birthday: '15/11/2015', class: 'Biên tập viên', work:'Tự soạn một bài thuyết trình về bản tin các quốc gia sau đó quay video gửi qua email Giám đốc', status:'Chưa hoàn thành', expiredate:'12h00 sáng, ngày 8/2/2026'}, 
 };
 
 // Hàm xóa sạch mọi dữ liệu người dùng khỏi localStorage
